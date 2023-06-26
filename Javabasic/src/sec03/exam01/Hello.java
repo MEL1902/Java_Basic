524f860dff12b288622d0ee13c56b1f177c68b06
MEL1902 <1902otamel@gmail.com>
2023-06-26 20:40:41 +0900

    2023.06.26 update

---
+++ Javabasic/src/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+/**
+ * 작성자: 이찬혁
+ * 작성일: 2023.06.26
+ * 기본적인 출력연습 예제코드
+ *
+ */
+public class Hello {
+    //프로그램 진입점
+    public static void main(String[] args) {
+        //콜솔에 출력하는 실행문
+        System.out.println("Hello world");
+    } // end of main
+} // end of class
