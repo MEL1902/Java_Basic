4d05dd5d42866ac560bcda132fa15b9400920c15
MEL1902 <1902otamel@gmail.com>
2023-07-31 23:35:31 +0900

    20230731 update

---
+++ 백준/JAVA10172/src/Main.java
@@ -0,0 +1,13 @@
+// 문제 출처:https://www.acmicpc.net/problem/10172
+// 이 문제는  출력 문구인 System.out.println 을 해준후 그대로 출력만 해주면 된다.
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
---
+++ 백준/Java9498/src/Main.java
@@ -0,0 +1,27 @@
+//문제 출처:https://www.acmicpc.net/problem/9498
+// 시험 점수를 입력받아 A,B,C,D 나머지F 등급을 출력하는 프로그램 작성이다.
+//입력받기 Scanner 와 if -ese if -else 문으로 작성을 해보겠다.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int D = scanner.nextInt();
+        int F = scanner.nextInt();
+
+        if (0<=100,100<=0 ); {
+            System.out.println("<=");
+        } else {
+            if ()
+        }
+
+
+      if(0<=100)
+    }
+}
